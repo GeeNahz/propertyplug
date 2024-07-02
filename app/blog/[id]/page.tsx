3dc94b02d711a70f5268280436be969a69f9e656
Godswill Nasara <godswillnlm@gmail.com>
2024-07-02 16:07:29 +0100

    update: started implementing the dynamic blog page

--- app/blog/[id]/page.tsx
+++ app/blog/[id]/page.tsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import { Navigator } from '@/components/Blog';
+import { BreadCrumbType } from "@/components/common/type";
+import Image from 'next/image';
 
-const Page = ({params}:{params:{id:string}}) => {
+import imgs5 from "@/components/Landing_Page/Blog_Post/images/ppt5.jpg";
+import Link from 'next/link';
+
+
+const Page = ({ params }: { params: { id: string } }) => {
+  const navigatorItems: BreadCrumbType[] = [<Link href="/blog">Blogs</Link>, params.id.replaceAll("-", " ")]
+
   return (
-    <section className='px-20 mt-16'>Blog Post {params.id}</section>
+    <section className="mt-28">
+      <div className="top-[63px] w-full fixed bg-white z-10">
+        <Navigator items={navigatorItems} />
+      </div>
+
+      <div className="px-20 py-20">
+        <div className="h-[450px] w-full overflow-hidden rounded-[30px]">
+          <Image width={1240} height={450} src={imgs5} alt={params.id} className="size-full object-cover bg-no-repeat bg-cover" />
+        </div>
+
+        Blog Post {params.id}
+      </div>
+    </section>
   )
 }
 
