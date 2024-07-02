37e7a6fb9191cfa535a7c4bd84a98b46269b8eac
Godswill Nasara <godswillnlm@gmail.com>
2024-07-02 15:29:28 +0100

    update: complete the blogs page

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import { BlogGalleries } from "@/components/common/data";
+import { BlogPosts, Navigator, Header, } from "@/components/Blog"
+import { BreadCrumbType } from "@/components/common/type";
+
 
 const Page = () => {
+  const navigatorItems: BreadCrumbType[] = ["Blogs"]
+
   return (
-    <section className='px-20 mt-16'>Blog Post</section>
+    <section className="mt-[63px]">
+      <div className="top-[63px] w-full sticky bg-white z-10">
+        <Navigator items={navigatorItems}/>
+        <Header />
+      </div>
+
+      <div className="px-20 w-full grid_wrapper mt-4 mb-24">
+        {BlogGalleries.map((v, i) => (
+          <BlogPosts key={i} post={v} />
+        ))}
+      </div>
+    </section>
   )
 }
 
