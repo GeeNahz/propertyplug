c86b4ae44c522efe50007da37bd369c5f80db5cc
Godswill Nasara <godswillnlm@gmail.com>
2024-07-02 15:24:45 +0100

    feature: create component that handles display of blog posts

---
+++ components/Blog/BlogPosts.tsx
@@ -0,0 +1,37 @@
+import { Button } from "@/components/common/button";
+import Image, { StaticImageData } from "next/image";
+
+type Props = {
+  time: string;
+  title: string;
+  img: StaticImageData;
+  grid: string;
+};
+
+const BlogPosts = ({ post }: { post: Props }) => {
+  return (
+    <div
+      className={`${post.grid} relative rounded-3xl h-full w-full border-white border-solid border-[2px] overflow-x-hidden`}
+    >
+      <Image
+        src={post.img}
+        alt="img"
+        fill
+        className="hover_img object-cover"
+      />
+      <div className="absolute top-0 w-full h-full left-0 transition duration-700 z-0 hover:translate-x-0 -translate-x-[100%]  linear_gradient flex flex-col justify-between p-3">
+        <div className="">
+          <small className="text-slate-50 text-xs font-normal">
+            {post.time}
+          </small>
+          <h4 className="text-white text-xl capitalize font-semibold">
+            {post.title}
+          </h4>
+        </div>
+        <Button title="read more" url={`/blog/${post.title.replaceAll(" ", "-")}`} />
+      </div>
+    </div>
+  );
+};
+
+export default BlogPosts;
