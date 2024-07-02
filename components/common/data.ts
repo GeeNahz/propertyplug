be7fbf1083bab3f5ae3867e717f29576d10f9831
Godswill Nasara <godswillnlm@gmail.com>
2024-07-02 11:48:24 +0100

    update: add more data for gallery with IDs

--- components/common/data.ts
+++ components/common/data.ts
@@ -1,4 +1,4 @@
-import { Feature, Gallery, NavLink, Social, Testimonial } from "./type";
+import { Feature, Gallery, GalleryWithID, NavLink, Social, Testimonial } from "./type";
 import imgs from "../Landing_Page/Blog_Post/images/ppt.jpg";
 import imgs1 from "../Landing_Page/Blog_Post/images/pp2.jpg";
 import imgs2 from "../Landing_Page/Blog_Post/images/ppt1.jpg";
@@ -6,6 +6,135 @@ import imgs3 from "../Landing_Page/Blog_Post/images/ppt3.jpg";
 import imgs4 from "../Landing_Page/Blog_Post/images/ppt4.jpg";
 import imgs5 from "../Landing_Page/Blog_Post/images/ppt5.jpg";
 import { FaLinkedinIn } from "react-icons/fa";
+
+export const BlogGalleries: GalleryWithID[] = [
+  {
+    id: 1,
+    time: "Monday, 7:45pm",
+    title: "what makes this house special?",
+    img: imgs,
+    grid:'big'
+  },
+  {
+    id: 2,
+    time: "Monday, 7:45pm",
+    title: "what makes this house special?",
+    img: imgs1,
+    grid:'tall'
+  },
+  {
+    id: 3,
+    time: "Monday, 7:45pm",
+    title: "what makes this house special?",
+    img: imgs2,
+    grid:'tall'
+  },
+  {
+    id: 4,
+    time: "Monday, 7:45pm",
+    title: "what makes this house special?",
+    img: imgs3,
+    grid:'tall'
+  },
+  {
+    id: 5,
+    time: "Monday, 7:45pm",
+    title: "what makes this house special?",
+    img: imgs4,
+    grid:'tall'
+  },
+  {
+    id: 6,
+    time: "Monday, 7:45pm",
+    title: "what makes this house special?",
+    img: imgs5,
+    grid:'big'
+  },
+  {
+    id: 7,
+    time: "Monday, 7:45pm",
+    title: "what makes this house special?",
+    img: imgs,
+    grid:'big'
+  },
+  {
+    id: 8,
+    time: "Monday, 7:45pm",
+    title: "what makes this house special?",
+    img: imgs1,
+    grid:'big'
+  },
+  {
+    id: 9,
+    time: "Monday, 7:45pm",
+    title: "what makes this house special?",
+    img: imgs2,
+    grid:'big'
+  },
+  {
+    id: 10,
+    time: "Monday, 7:45pm",
+    title: "what makes this house special?",
+    img: imgs3,
+    grid:'tall'
+  },
+  {
+    id: 11,
+    time: "Monday, 7:45pm",
+    title: "what makes this house special?",
+    img: imgs4,
+    grid:'tall'
+  },
+  {
+    id: 12,
+    time: "Monday, 7:45pm",
+    title: "what makes this house special?",
+    img: imgs5,
+    grid:'tall'
+  },
+  {
+    id: 13,
+    time: "Monday, 7:45pm",
+    title: "what makes this house special?",
+    img: imgs,
+    grid:'tall'
+  },
+  {
+    id: 14,
+    time: "Monday, 7:45pm",
+    title: "what makes this house special?",
+    img: imgs1,
+    grid:'tall'
+  },
+  {
+    id: 15,
+    time: "Monday, 7:45pm",
+    title: "what makes this house special?",
+    img: imgs2,
+    grid:'tall'
+  },
+  {
+    id: 16,
+    time: "Monday, 7:45pm",
+    title: "what makes this house special?",
+    img: imgs3,
+    grid:'big'
+  },
+  {
+    id: 17,
+    time: "Monday, 7:45pm",
+    title: "what makes this house special?",
+    img: imgs4,
+    grid:'big'
+  },
+  {
+    id: 18,
+    time: "Monday, 7:45pm",
+    title: "what makes this house special?",
+    img: imgs5,
+    grid:'big'
+  },
+]
 
 export const blogGallery: Gallery = [
   {
