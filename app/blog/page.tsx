746994fbb40bac9c6f6d32a112f74e653655757a
Godswill Nasara <godswillnlm@gmail.com>
2024-07-04 13:34:19 +0100

    refactor: use BlogPosts component to display blog posts grid

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,8 +1,6 @@
 import { BlogGalleries } from "@/components/common/data";
-import { Navigator, Header, } from "@/components/Blog"
+import { Navigator, Header, BlogPosts, } from "@/components/Blog"
 import { BreadCrumbType } from "@/components/common/type";
-import { Button } from "@/components/common/button";
-import Image from "next/image";
 
 
 const Page = () => {
@@ -21,23 +19,7 @@ const Page = () => {
             key={i}
             className={`${v.grid} relative rounded-3xl h-full w-full border-white border-solid border-[2px] overflow-x-hidden`}
           >
-            <Image
-              src={v.img}
-              alt="img"
-              fill
-              className="hover_img object-cover"
-            />
-            <div className="absolute top-0 w-full h-full left-0 transition duration-700 z-0 hover:translate-x-0 -translate-x-[100%]  linear_gradient flex flex-col justify-between p-3">
-              <div className="">
-                <small className="text-slate-50 text-xs font-normal">
-                  {v.time}
-                </small>
-                <h4 className="text-white text-xl capitalize font-semibold">
-                  {v.title}
-                </h4>
-              </div>
-              <Button title="read more" url={`/blog/${v.title.replaceAll(" ", "-")}`} />
-            </div>
+            <BlogPosts post={v} />
           </div>
         ))}
       </div>
