14a0a427ab3c1ae26d88865fef2d505cdef32378
Godswill Nasara <godswillnlm@gmail.com>
2024-07-04 13:17:04 +0100

    refactor: brought content from BlogPosts component to the main page

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,6 +1,8 @@
 import { BlogGalleries } from "@/components/common/data";
-import { BlogPosts, Navigator, Header, } from "@/components/Blog"
+import { Navigator, Header, } from "@/components/Blog"
 import { BreadCrumbType } from "@/components/common/type";
+import { Button } from "@/components/common/button";
+import Image from "next/image";
 
 
 const Page = () => {
@@ -9,13 +11,34 @@ const Page = () => {
   return (
     <section className="mt-[63px]">
       <div className="top-[63px] w-full sticky bg-white z-10">
-        <Navigator items={navigatorItems}/>
+        <Navigator items={navigatorItems} />
         <Header />
       </div>
 
       <div className="px-20 w-full grid_wrapper mt-4 mb-24">
         {BlogGalleries.map((v, i) => (
-          <BlogPosts key={i} post={v} />
+          <div
+            key={i}
+            className={`${v.grid} relative rounded-3xl h-full w-full border-white border-solid border-[2px] overflow-x-hidden`}
+          >
+            <Image
+              src={v.img}
+              alt="img"
+              fill
+              className="hover_img object-cover"
+            />
+            <div className="absolute top-0 w-full h-full left-0 transition duration-700 z-0 hover:translate-x-0 -translate-x-[100%]  linear_gradient flex flex-col justify-between p-3">
+              <div className="">
+                <small className="text-slate-50 text-xs font-normal">
+                  {v.time}
+                </small>
+                <h4 className="text-white text-xl capitalize font-semibold">
+                  {v.title}
+                </h4>
+              </div>
+              <Button title="read more" url={`/blog/${v.title.replaceAll(" ", "-")}`} />
+            </div>
+          </div>
         ))}
       </div>
     </section>
