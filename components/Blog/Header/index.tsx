521e9462ff168c9532201a54863b63d59daa5f7e
Godswill Nasara <godswillnlm@gmail.com>
2024-07-04 16:36:03 +0100

    update: render Navigator component

--- components/Blog/Header/index.tsx
+++ components/Blog/Header/index.tsx
@@ -1,17 +1,33 @@
+"use client"
+
+
+import { useState } from "react";
 import SearchFilters from "./SearchFilters";
 import Socials from "./Socials";
 import Title from "./Title";
+import { BreadcrumbItemType, BreadcrumbSeparatorType } from "antd/es/breadcrumb/Breadcrumb";
+import { Navigator } from "..";
 
 const Header = () => {
+  const [
+    navigatorItems,
+    setNavigatorItems
+  ] = useState<Partial<BreadcrumbItemType & BreadcrumbSeparatorType>[]>([
+    { title: "Home", path: "/", },
+    { title: "Blogs", path: "/blog" },
+  ]);
   return (
-    <section className="px-20 py-12">
-      <div className="section-title flex justify-between items-start w-full mb-11">
-        <Title />
+    <>
+      <Navigator items={navigatorItems} /> {/* brought this here to make this the client component */}
+      <section className="px-20 py-12">
+        <div className="section-title flex justify-between items-start w-full mb-11">
+          <Title />
 
-        <Socials />
-      </div>
-       <SearchFilters />
-    </section>
+          <Socials />
+        </div>
+        <SearchFilters />
+      </section>
+    </>
   );
 };
 
