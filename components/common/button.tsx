63390d601b241bd6945120def9ff35c668eb7b50
Godswill Nasara <godswillnlm@gmail.com>
2024-07-07 21:00:01 +0100

    update: setup button to change bg color based on the current path

--- components/common/button.tsx
+++ components/common/button.tsx
@@ -1,25 +1,40 @@
 'use client'
 import { useStateContext } from "@/hook/statecontext"
+import clsx from "clsx"
+import { usePathname } from "next/navigation";
 import Link from "next/link"
 
 type ButtonProp = {
-  title:string,
-  url?:string
+  title: string,
+  url?: string
 }
-export const Button = ({title, url}:ButtonProp) => {
-  const {setModal} = useStateContext()
+export const Button = ({ title, url }: ButtonProp) => {
+  const { setModal } = useStateContext()
+  const isConsultancy = title === 'book consultancy';
+
   const handleClick = () => {
-    if(title === 'book consultancy'){
+    if (isConsultancy) {
       setModal(true)
     }
     return
   }
 
+  const path = usePathname();
+
+  const btnClass = clsx(
+    "p-3 rounded-[32px] font-medium text-xs capitalize w-max",
+    {
+      "bg-white text-ui-dark": (path === "/" && isConsultancy),
+      "bg-ui-dark text-white": ((path !== "/") && isConsultancy),
+      "bg-white": ((path !== "/") && !isConsultancy),
+    },
+  );
+
   return (
     <a href={url} className="flex w-max">
-      <button className='p-3 rounded-[32px] bg-white font-medium text-xs capitalize w-max' onClick={handleClick}>
-          {title}
+      <button className={btnClass} onClick={handleClick}>
+        {title}
       </button>
     </a>
-    )
+  )
 }
