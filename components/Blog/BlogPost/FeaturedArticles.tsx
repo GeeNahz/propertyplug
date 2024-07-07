bc9e0f12ec8e77397d0dea7d4652b39b5741c73d
Godswill Nasara <godswillnlm@gmail.com>
2024-07-07 21:01:46 +0100

    refactor: switch to already defined Header component to display section heading

--- components/Blog/BlogPost/FeaturedArticles.tsx
+++ components/Blog/BlogPost/FeaturedArticles.tsx
@@ -4,6 +4,7 @@ import { GalleryWithID } from "@/components/common/type";
 import { Pagination, PaginationProps, ConfigProvider } from "antd";
 import { useState } from "react";
 import imgs3 from "@/components/Landing_Page/Blog_Post/images/ppt3.jpg";
+import Header from "@/components/common/header";
 import BlogPosts from "../BlogPosts";
 import BlogPostsGrid from "../BlogPostsGrid";
 
@@ -49,10 +50,10 @@ const FeaturedArticles = () => {
   return (
     <section>
       <div className="header flex items-center justify-between px-32 py-8">
-        <div className="texts">
-          <p className="font-medium text-ui-dark text-3xl capitalize mb-2">featured articles</p>
-          <p className="text-ui-dark font-light text-sm tracking-[0.42px]">Explore explicit Content Just for you</p>
-        </div>
+        <Header
+          title="Featured Articles"
+          desc="Explore explicit Content Just for you"
+        />
 
         <ConfigProvider
           theme={{
