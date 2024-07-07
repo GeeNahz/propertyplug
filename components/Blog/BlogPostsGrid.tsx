bc8a8ec6506b0757df0e308c9b4376191d5204b0
Godswill Nasara <godswillnlm@gmail.com>
2024-07-07 19:36:12 +0100

    feature: create reusable component that renders posts in grids

---
+++ components/Blog/BlogPostsGrid.tsx
@@ -0,0 +1,23 @@
+import { GalleryWithID } from "../common/type";
+import BlogPosts from "./BlogPosts";
+
+type Props = {
+  posts: GalleryWithID[];
+};
+
+const BlogPostsGrid = ({ posts }: Props) => {
+  return (
+    <div className="px-20 w-full grid_wrapper mt-4 mb-24">
+      {posts.map((v, i) => (
+        <div
+          key={i}
+          className={`${v.grid} relative rounded-3xl h-full w-full border-white border-solid border-[2px] overflow-x-hidden`}
+        >
+          <BlogPosts post={v} />
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default BlogPostsGrid;
