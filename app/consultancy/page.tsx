474329045031db4ea253abb873bcc4157a404e8d
Godswill Nasara <godswillnlm@gmail.com>
2024-07-18 01:23:53 +0100

    update: utilise loading component as fallback to suspense component

--- app/consultancy/page.tsx
+++ app/consultancy/page.tsx
@@ -1,10 +1,11 @@
+import Loading from '@/components/common/loader'
 import { FormPage } from '@/components/Consultancy'
 import { Suspense } from 'react'
 
 const Page = () => {
   return (
     <div className="min-h-screen pt-[63px]">
-      <Suspense fallback={<p>Please wait...</p>}>
+      <Suspense fallback={<Loading />}>
         <FormPage />
       </Suspense>
     </div>
