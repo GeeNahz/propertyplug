77a84758a3d1853de5423c9188d8c5616bd8acbc
Godswill Nasara <godswillnlm@gmail.com>
2024-07-18 12:25:21 +0100

    update: make the hero section on landing page responsive

--- components/Landing_Page/Hero_Section/heroDropDown.tsx
+++ components/Landing_Page/Hero_Section/heroDropDown.tsx
@@ -2,31 +2,32 @@
 import React, { useState } from "react";
 import { MdArrowDropDown } from "react-icons/md";
 import { Button } from "../../common/button";
+import Link from "next/link";
 
 export const DropList = () => {
-    const [drop,setDrop] = useState(false)
+  const [drop, setDrop] = useState(false)
   return (
-    <div className="flex py-1 px-2 rounded-[32px] border-[3px] border-white border-solid mt-6 gap-8 relative">
-      <small className="capitalize flex items-center gap-3 text-zinc-400 cursor-pointer" onClick={() => setDrop(!drop)}>
+    <div className="flex py-1 pr-1 pl-2 md:px-2 rounded-[25px] md:rounded-[32px] border md:border-[3px] border-white border-solid mt-6 gap-1 md:gap-8 relative">
+      <small className="text-xs md:text-inherit capitalize flex items-center gap-1 md:gap-3 text-zinc-400 cursor-pointer" onClick={() => setDrop(!drop)}>
         what are you looking for?{" "}
-        <MdArrowDropDown size={20} className={`transition ${drop? 'rotate-0':'rotate-180'}`} />
+        <MdArrowDropDown size={20} className={`transition ${drop ? 'rotate-0' : 'rotate-180'}`} />
         {drop && <DropDown />}
       </small>
-      <Button title="book consultancy" />
+      <Button url="/consultancy" title="book consultancy" />
     </div>
   );
 };
 
 const DropDown = () => {
-    return (
-      <div className="drop__fade absolute top-16 left-0 right-0 p-3 w-64 border-[2px] border-white border-solid rounded-2xl bg-gray-600/15 flex flex-col gap-3" onClick={(e) => e.stopPropagation()}>
-        {["buy property", "sell property", "rent property"].map((v: any) => {
-          return (
-            <button key={v} className="text-white font-medium text-lg capitalize py-2 text-center bg-gray-500/15 border-transparent border border-solid hover:border-white rounded-3xl">
-              {v}
-            </button>
-          );
-        })}
-      </div>
-    );
-  };
+  return (
+    <div className="drop__fade absolute top-16 left-0 right-0 p-3 w-64 border md:border-[2px] border-white border-solid rounded-2xl bg-gray-600/15 flex flex-col gap-3" onClick={(e) => e.stopPropagation()}>
+      {[{ title: "buy property", path: '/consultancy?tab=buy' }, { title: "sell property", path: '/consultancy?tab=sell' }, { title: "rent property", path: '/consultancy?tab=rent' },].map((v, index) => {
+        return (
+          <Link href={v.path} key={index} className="text-white font-medium text-sm md:text-lg capitalize py-2 text-center bg-gray-500/15 border-transparent border border-solid hover:border-white transition-all rounded-3xl">
+            {v.title}
+          </Link>
+        );
+      })}
+    </div>
+  );
+};
--- components/Landing_Page/Hero_Section/index.tsx
+++ components/Landing_Page/Hero_Section/index.tsx
@@ -2,20 +2,20 @@ import { DropList } from "@/components/Landing_Page/Hero_Section/heroDropDown";
 
 const HeroSection = () => {
   return (
-    <section className="h-screen bg_hero flex items-center">
+    <section className="h-screen bg_hero flex items-center px-7 md:px-0">
       <div className="h-full bg-transparent w-full flex flex-col justify-center items-center py-3 backdrop-blur-sm">
-        <small className="text-zinc-400 text-sm">
+        <small className="text-zinc-400 text-xs md:text-sm">
           Excellence from Abuja Nigeria
         </small>
-        <h1 className="text-white text-center font-bold text-6xl my-2">
+        <h1 className="text-white text-center font-bold text-4xl md:text-6xl my-2">
           The <span className="text-[#FF4A55]">Ultimate</span> Real Estate{" "}
           <br /> Experience
         </h1>
-        <small className="text-zinc-400 text-base font-medium text-center mt-2">
+        <small className="text-zinc-400 text-sm md:text-base font-medium text-center mt-2">
           We prioritize our clients&apos; needs and are committed to <br />{" "}
           delivering tangible results.
         </small>
-        <DropList/>
+        <DropList />
       </div>
     </section>
   );
