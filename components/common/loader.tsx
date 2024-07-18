136ab79960c1f8f189a43108c6faf1fcd6d916c2
Godswill Nasara <godswillnlm@gmail.com>
2024-07-18 01:22:09 +0100

    feature: create a loading component

---
+++ components/common/loader.tsx
@@ -0,0 +1,11 @@
+import { Loader } from 'lucide-react'
+
+const Loading = () => {
+  return (
+    <div className='flex justify-center items-center w-full h-screen bg-white'>
+      <Loader size={40} color="#007D53" />
+    </div>
+  )
+}
+
+export default Loading
