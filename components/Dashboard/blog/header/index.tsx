1b6e063c79b7786a3a24f6a95c47c4514db8b941
Godswill Nasara <godswillnlm@gmail.com>
2024-07-19 23:10:29 +0100

    refactor: change the create btn from a button el to a Link el

--- components/Dashboard/blog/header/index.tsx
+++ components/Dashboard/blog/header/index.tsx
@@ -3,6 +3,7 @@
 import { useState } from "react";
 import { Navigator } from "@/components/Blog";
 import { BreadcrumbItemType, BreadcrumbSeparatorType } from "antd/es/breadcrumb/Breadcrumb";
+import Link from "next/link";
 
 export default function Header() {
   const [
@@ -18,7 +19,7 @@ export default function Header() {
     <div className="w-full flex items-center justify-between">
       <Navigator items={navigatorItems} />
 
-      <button className="bg-ui-dark capitalize font-semibold text-sm py-3 px-6 text-white rounded-full">Create Blog</button>
+      <Link href={'/dashboard/blogs/create'} className="bg-ui-dark capitalize font-semibold text-sm py-3 px-6 text-white rounded-full">Create Blog</Link>
     </div>
   )
 }
