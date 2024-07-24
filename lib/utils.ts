4a7fd17fb17b0e2e84ae021527ddeb28df1878de
Godswill Nasara <godswillnlm@gmail.com>
2024-07-24 21:03:24 +0100

    feature: create utiliites to decrypt and validate tokens for auth

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,35 @@
-import { type ClassValue, clsx } from "clsx"
+import type { ClassValue } from "clsx"
+import clsx from 'clsx'
 import { twMerge } from "tailwind-merge"
+
+import {jwtDecode} from 'jwt-decode'
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function decrypt(token: string) {
+  return jwtDecode(token)
+}
+
+export function validateToken(token: string) {
+  let now = new Date().getTime()
+  let isValid = false
+
+  let decrypted = decrypt(token)
+  try {
+    isValid = (decrypted && decrypted.exp) ? ((decrypted.exp * 1000) > now) : false
+    return isValid
+  } catch(err: any){
+    console.log('Decryption err: ', err.message)
+  }
+
+  return isValid
+}
+
+// export async function getFileBuffer(file: File) {
+//   const arrayBuffer = await file.arrayBuffer()
+//   const buffer = new Uint8Array(arrayBuffer)
+//
+//   return buffer
+// }
