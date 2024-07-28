fc435ff9259bede71c58e2367026ccb5597b5223
Godswill Nasara <godswillnlm@gmail.com>
2024-07-28 20:00:43 +0100

    update: add fallback message for empty blog posts

--- components/Dashboard/blog/tiles/Tiles.tsx
+++ components/Dashboard/blog/tiles/Tiles.tsx
@@ -8,10 +8,13 @@ export default function Tiles({ posts }: { posts: TBlogPost[] }) {
 
   return (
     <div className="space-y-5">
-      {
-        posts.map((post) => (
+      {posts
+        ? posts.map((post) => (
           <Tile key={post.id} post={post} />
         ))
+        : (<div className="w-full p-10 rounded-[20px] bg-white flex gap-6 items-center">
+          <p className="text-gray-300 text-xl font-semibold">No available blog</p>
+        </div>)
       }
     </div>
   )
