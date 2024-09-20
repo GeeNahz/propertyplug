4c421a84eb27113d4b250f05f3a7a2b5f3f107a4
Godswill Nasara <godswillnlm@gmail.com>
2024-09-20 17:40:18 +0100

    fix: resolve blogs grid collapse

--- components/Landing_Page/Blog_Post/blogList.tsx
+++ components/Landing_Page/Blog_Post/blogList.tsx
@@ -14,35 +14,6 @@ export default async function BlogList() {
   const blogs = response.result as TBlogPost[]
 
   return (
-    <div className="w-full grid_wrapper mt-4">
-      <BlogPostsGrid posts={blogs.slice(0, 4)} />
-
-      {/* blogs.slice(0, 4).map((v: TBlogPost, i: number) => {
-        return (
-          <div
-            key={i}
-            className={`relative rounded-3xl h-full w-full border-white border-solid border-[2px] overflow-x-hidden`}
-          >
-            <Image
-              src={v.thumbnail || v.backgroundImage as string}
-              alt="img"
-              fill
-              className="hover_img object-cover"
-            />
-            <div className="absolute top-0 w-full h-full left-0 transition translate-x-0 duration-700 z-0 linear_gradient flex flex-col justify-between p-3">
-              <div className="">
-                <small className="text-slate-50 text-xs font-normal">
-                  <DateConverter date={v.createdAt} />
-                </small>
-                <h4 className="text-white text-xl capitalize font-semibold">
-                  {v.title.length > 30 ? `${v.title.slice(0, 30).trim()}...` : v.title}
-                </h4>
-              </div>
-              <Button title="read" url={`/blog/${v.slug}`} />
-            </div>
-          </div>
-        );
-      }) */}
-    </div>
+    <BlogPostsGrid posts={blogs.slice(0, 4)} />
   );
 }
